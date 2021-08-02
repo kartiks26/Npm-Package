abcd78a7ce2c553da17f74647fc62f078129fee5
pratikartik <kartikshikhare26@gmail.com>
2021-08-03 00:46:30 +0530

    custom react template

---
+++ template/src/store.js
@@ -0,0 +1,6 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/index";
+
+// Note: this API requires redux@>=3.1.0
+const store = createStore(rootReducer);
